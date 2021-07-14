eed821b62ebddf4828d33ec389e260c0bbfb34a3
warjules <55206689+warjules@users.noreply.github.com>
2021-07-14 18:35:31 +0100

    loupe faq

--- assets/js/faq.js
+++ assets/js/faq.js
@@ -1,13 +1,86 @@
-var options = {
-	valueNames: ['question', 'answer']
-  };
-  var faqList = new List('faq-list', options);
-  var noItems = $('<li id="Aucun résultat. Choisissez un mot clef."><h3 class="question">No items found</h3></li>');
-  
-  faqList.on('updated', function(list) {
-	if (list.matchingItems.length == 0) {
-	  $(list.list).append(noItems);
-	} else {
-	  noItems.detach();
-	}
-  });
+'use strict';
+
+
+	// search & highlight
+
+	;( function( $, window, document, undefined )
+	{
+		var $container = $( '.faq' );
+		if( !$container.length ) return true;
+
+		var $input			= $container.find( 'input' ),
+			$notfound		= $container.find( '.faq__notfound' ),
+			$items			= $container.find( '> ul > li' ),
+			$item			= $(),
+			itemsIndexed	= [];
+
+		$items.each( function()
+		{
+			itemsIndexed.push( $( this ).text().replace( /\s{2,}/g, ' ' ).toLowerCase() );
+		});
+
+		$input.on( 'keyup', function( e )
+		{
+			if( e.keyCode == 13 ) // enter
+			{
+				$input.trigger( 'blur' );
+				return true;
+			}
+
+			$items.each( function()
+			{
+				$item = $( this );
+				$item.html( $item.html().replace( /<span class="highlight">([^<]+)<\/span>/gi, '$1' ) );
+			});
+
+			var searchVal = $.trim( $input.val() ).toLowerCase();
+			if( searchVal.length )
+			{
+				for( var i in itemsIndexed )
+				{
+					$item = $items.eq( i );
+					if( itemsIndexed[ i ].indexOf( searchVal ) != -1 )
+						$item.removeClass( 'is-hidden' ).html( $item.html().replace( new RegExp( searchVal+'(?!([^<]+)?>)', 'gi' ), '<span class="highlight">$&</span>' ) );
+					else
+						$item.addClass( 'is-hidden' );
+				}
+			}
+			else $items.removeClass( 'is-hidden' );
+
+			$notfound.toggleClass( 'is-visible', $items.not( '.is-hidden' ).length == 0 );
+		});
+	})( jQuery, window, document );
+
+
+	// toggling items on title press
+
+	;( function( $, window, document, undefined )
+	{
+		$( document ).on( 'click', '.faq h2 a', function( e )
+		{
+			e.preventDefault();
+			$( this ).parents( 'li' ).toggleClass( 'is-active' );
+		});
+	})( jQuery, window, document );
+
+
+	// auto-show item content when show results reduces to single
+
+	;( function( $, window, document, undefined )
+	{
+		var $container = $( '.faq' );
+		if( !$container.length ) return true;
+
+		var $input		= $container.find( 'input' ),
+			$items		= $container.find( '> ul > li' ),
+			$item		= $();
+
+		$input.on( 'keyup', function()
+		{
+			$item = $items.not( '.is-hidden' );
+			if( $item.length == 1 )
+				$item.addClass( 'js--autoshown is-active' );
+			else
+				$items.filter( '.js--autoshown' ).removeClass( 'js--autoshown is-active' );
+		});
+	})( jQuery, window, document );
--- assets/js/sidepannel.js
+++ assets/js/sidepannel.js
@@ -1,17 +1,3 @@
-var options = {
-	valueNames: ['question', 'answer']
-  };
-  var faqList = new List('faq-list', options);
-  var noItems = $('<li id="Aucun résultat. Choisissez un mot clef."><h3 class="question">No items found</h3></li>');
-  
-  faqList.on('updated', function(list) {
-	if (list.matchingItems.length == 0) {
-	  $(list.list).append(noItems);
-	} else {
-	  noItems.detach();
-	}
-  });
-
 
 
 /* Set the width of the sidebar pour les tarifs to 250px (show it) */
