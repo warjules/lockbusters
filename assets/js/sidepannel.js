6601d4acad40596c7abb6c7c613408e2197e4080
warjules <55206689+warjules@users.noreply.github.com>
2021-10-27 14:09:29 +0100

    yes

--- assets/js/sidepannel.js
+++ assets/js/sidepannel.js
@@ -1,14 +1,14 @@
 
 
 /* Set the width of the sidebar pour les tarifs to 250px (show it) */
-function openNav() {
-	document.getElementById("mySidepanel").style.width = "300px";
+function openNav2() {
   document.getElementById("mySidepanel2").style.width = "300px";
-  document.getElementById("wrapper").style.opacity = "0.1";
+  document.getElementById("wrapper").style.opacity = "0.1";  
+}
 
-  
-
-
+  function openNav() {
+	document.getElementById("mySidepanel").style.width = "300px";
+  document.getElementById("wrapper").style.opacity = "0.1";  
   }
   
   /* Set the width of the sidebar to 0 (hide it) */
